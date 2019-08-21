12bf6b5258f9665e2346c98234a05a0c48940a86
Ana Davis <anac.davis@gmail.com>
2019-08-20 18:15:17 -0700

    variables added

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var $ = function (id) {
+    return document.getElementById(id);
+}
+
+
+var words = ["shark", "dolphin", "scubadiving", "snorkel", "whale", "fish"]
+var wordChoice = Math.floor(Math.random()*5);
+var answer = words[wordChoice];
+var myLength = answer.length;
+var display = [myLength];
+var win = myLength;
+var letters = answer.split ('');
+var attemptsleft = 7;
+var output = "";
+var userLetter = "";
+
+
+
+
+
+
+// Functions
+var setup = function() {
+    for (var i=0; i<answer.lenght; i++)
+    display [i] = "_ ";
+    output = output + display[i];
+}
+ document.getElementById("words").innerHTML = output;
+ output ="";   
+
+ window.onload = function () {
+     setup ();
+
+ }
